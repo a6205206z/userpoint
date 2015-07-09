0799e469de7e217befc876cbd0e490cae2a2b5a4
cheng cheng <c.j.king@163.com>
2015-07-09 17:20:11 +0800

    update migrate

--- db/migrate/001_setup.rb
+++ db/migrate/001_setup.rb
@@ -1,14 +1,15 @@
 # User Point project
 #
 #file: setup.rb
+#author: cean cheng
+#email: cean.ch@gmail.com
 #description: setup the system data base
-
+#
 
 
 class Setup < ActiveRecord::Migration
 	def self.up
-		create_table :userinfo do |t|
-			t.column :user_id, :string, null => false
+		create_table :user_info do |t|
 			t.column :login_name, :string, :limit => 30, :default => "", :null => false
 			t.column :login_password, :string, :default => "", :null =>false
 			t.column :real_name, :string, :default => "", :null => false
@@ -16,11 +17,13 @@ class Setup < ActiveRecord::Migration
 			t.column :sex, :integer, :limit => 2, :default => 0, :null => false
 			t.column :create_time, :datetime, :null => false
 			t.column :user_point, :integer, :default => 0, :null => false
-			t.column :user_money, :decimal, :default => 0.0 :null => false
+			t.column :user_money, :decimal, :default => 0.0, :null => false, precision: 5, scale: 2
 		end
 
 		create_table :user_address do |t|
-			t.column :user_id, :string, :defualt => "", :null => false
+			t.column :user_id, :integer, :defualt => 0, :null => false
+			t.column :real_name, :string, :default => "", :null => false
+			t.column :mobile, :string, :default => "", :null => false
 			t.column :country, :string, :defualt => "", :null => false
 			t.column :province, :string, :defualt => "", :null => false
 			t.column :city, :string, :defualt => "", :null => false
@@ -29,27 +32,25 @@ class Setup < ActiveRecord::Migration
 		end
 
 		create_table :user_point_io do |t|
-			t.column :user_id, :string, :defualt => "", :null => false
+			t.column :user_id, :integer, :defualt => 0, :null => false
 			t.column :point, :integer, :default => 0, :null =>false
 			t.column :remarks, :string, :limit => 125, :defualt => "", :null => false
 			t.column :status, :integer, :default => 0, :null => false
 			t.column :operate_time, :datetime, :null => false
-			t.column :operate_user_id,  :string, :default => "system", :null => false
-			end
+			t.column :operate_user_id,  :integer, :default => 0, :null => false
 		end
 
 		create_table :user_money_io do |t|
-			t.column :user_id, :string, :defualt => "", :null => false
-			t.column :money, :decimal, :defualt => 0.0, :null => false, scale: 2
+			t.column :user_id, :integer, :defualt => 0, :null => false
+			t.column :money, :decimal, :defualt => 0.00, :null => false, precision: 5, scale: 2
 			t.column :remarks, :string, :limit => 125, :defualt => "", :null => false
 			t.column :status, :integer, :default => 0, :null => false
 			t.column :operate_time, :datetime, :null => false
-			t.column :operate_user_id, :string, :default => "system", :null => false
+			t.column :operate_user_id, :integer, :default => 0, :null => false
 		end
 
 		create_table :prdocut do |t|
-			t.column :product_id, :string, :defualt => "", :null => false
-			t.column :sales_point, :integer, :defualt => 0， :null => false
+			t.column :sales_point, :integer, :defualt => 0, :null => false
 			t.column :original_point, :integer, :default => 0, :null => false
 			t.column :product_img_url, :string, :limit => 60, :default => "", :null => false
 			t.column :inventory, :integer, :default => 0, :null => false
@@ -57,19 +58,41 @@ class Setup < ActiveRecord::Migration
 		end
 
 		create_table :order do |t|
-			t.column :order_id， :string, :default => "", :null => false
+			t.column :user_id, :integer, :default => 0, :null => false
 			t.column :order_point, :integer, :default => 0, :null => false
 			t.column :status, :integer, :default => 0, :null => false
 			t.column :create_time, :datetime, :null => false
-			
+			t.column :remarks, :string, :limit => 200
+		end
+
+		create_table :order_shipping do |t|
+			t.column :order_id, :integer, :default => 0, :null => false
+			t.column :real_name, :string, :default => "", :null => false
+			t.column :mobile, :string, :default => "", :null => false
+			t.column :country, :string, :defualt => "", :null => false
+			t.column :province, :string, :defualt => "", :null => false
+			t.column :city, :string, :defualt => "", :null => false
+			t.column :post_code, :string, :defualt => "", :null => false
+			t.column :address, :string, :defualt => "", :null => false
+		end
+
+		create_table :order_item do |t|
+			t.column :order_id, :integer, :default => 0, :null => false
+			t.column :product_id, :integer, :default => 0, :null => false
+			t.column :pre_point, :integer, :default => 0, :null => false
+			t.column :count, :integer, :default => 0, :null => false
+			t.column :total_point, :integer, :default => 0, :null => false
 		end
 	end
 
 	def self.down
-		drop_table :userinfo
+		drop_table :user_info
 		drop_table :user_address
-		drop_table :user_point
+		drop_table :user_point_io
 		drop_table :user_money_io
 		drop_table :prdocut
+		drop_table :order
+		drop_table :order_shipping
+		drop_table :order_item
 	end
 end

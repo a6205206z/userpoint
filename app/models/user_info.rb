a721ebc2b697b9932e65bf83c1845f0d207ac8e3
cheng cheng <c.j.king@163.com>
2015-07-12 21:08:34 +0800

    add reg user and money

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -6,4 +6,10 @@ class UserInfo < ActiveRecord::Base
 	def self.hash_password(clear_password)
 		Digest::SHA1.hexdigest(clear_password || "")
 	end
+
+	def self.get_my_code(login_name)
+		Digest::SHA1.hexdigest(login_name || "")
+	end
+
+	validates :login_name, :presence => true, :uniqueness => {:message => "This login_name has been used"}
 end

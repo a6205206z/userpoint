ff8114055761e98549d162378ef6d9099df83cfb
keke <liuke8399@163.com>
2015-07-20 22:09:01 +0800

    更改样式

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-RAILS_ROOT = 'C:/www/UserPoint'
+RAILS_ROOT = '/vagrant/learnRails/userpoint'
 
 module UserPoint
   class Application < Rails::Application
